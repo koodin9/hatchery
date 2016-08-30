4e89abbc77edbcccbb4b274633cb3714642ae417
DuckG <DuckG@192.168.0.138>
2016-08-30 21:59:53 +0900

    DataTypeWrapper항목추

--- src/test/DataTypeWrapper.java
+++ src/test/DataTypeWrapper.java
@@ -4,4 +4,5 @@ public class DataTypeWrapper {
 	private String attributeType; //이건 IntWritable인지 뭔지 
 	private String attribute;// 이건 컬럼네임, 예를들어 age, height 이런
 	private int index; //로우 파일에서 인덱스 몇번째 것인
+	private String method;//처리 뭐할꺼야? -성훈 
 }
